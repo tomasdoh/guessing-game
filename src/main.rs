f36829d63de3a6b8b77b28a0be08ecc5eb850704
Tomas Doherty <tomasdoh1@gmail.com>
2019-02-06 01:02:00 +0000

    Added break to loop; ignores non-number inputs

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,20 @@ fn main() {
             .ok()
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse()
-            .ok()
-            .expect("Please type a number!");
+        let guess: u32 =  match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less    => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal   => println!("You win!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
